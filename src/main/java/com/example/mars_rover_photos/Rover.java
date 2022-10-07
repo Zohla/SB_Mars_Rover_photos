b73f21dad5367e7f41688ad8cb398f97d760b836
Maren Lilleberre <marenlilleberre@gmail.com>
2022-10-07 14:13:47 +0200

    add getters/setters, toString and @JsonProperty for keys with underscores

--- src/main/java/com/example/mars_rover_photos/Rover.java
+++
@@ -1,9 +0,0 @@
-package com.example.mars_rover_photos;
-
-public class Rover {
-    private int roverId;
-    private String roverName;
-    private String landingDate;
-    private String launchDate;
-    private String status;
-}
---
+++ src/main/java/com/example/mars_rover_photos/response/Rover.java
@@ -0,0 +1,64 @@
+package com.example.mars_rover_photos.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Rover {
+    private int id;
+    private String name;
+    @JsonProperty("landing_date")
+    private String landingDate;
+    @JsonProperty("launch_date")
+    private String launchDate;
+    private String status;
+
+    @Override
+    public String toString() {
+        return "Rover{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", landingDate='" + landingDate + '\'' +
+                ", launchDate=" + launchDate +
+                ", status='" + status + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLandingDate() {
+        return landingDate;
+    }
+
+    public void setLandingDate(String landingDate) {
+        this.landingDate = landingDate;
+    }
+
+    public String getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(String launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
