12697c3dac0781fde2f390b3abad5b89260a1522
Maren Lilleberre <marenlilleberre@gmail.com>
2022-10-07 21:57:41 +0200

    remove test data

--- src/test/java/com/example/mars_rover_photos/MarsRoverAPITest.java
+++ src/test/java/com/example/mars_rover_photos/MarsRoverAPITest.java
@@ -1,6 +1,6 @@
 package com.example.mars_rover_photos;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
+import com.example.mars_rover_photos.response.ApiResponseRoverPhotos;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -9,9 +9,5 @@ public class MarsRoverAPITest {
 
     @Test
     public void smallTest() {
-        RestTemplate rt = new RestTemplate();
-
-        ResponseEntity<ApiResponseRoverPhotos> response = rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?id=118330&sol=100&api_key=7C3efywnhiZulYlDVdUYW9M5u71tIS22nmcak61h", ApiResponseRoverPhotos.class);
-        System.out.println(response.getBody());
     }
 }
