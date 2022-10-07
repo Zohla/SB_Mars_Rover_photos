ed4f5590d1b0511cecb4a44f349306e9c24d0b06
Maren Lilleberre <marenlilleberre@gmail.com>
2022-10-07 21:57:20 +0200

    remove test data, add api response

--- src/main/java/com/example/mars_rover_photos/web/HomeController.java
+++ src/main/java/com/example/mars_rover_photos/web/HomeController.java
@@ -1,16 +1,24 @@
 package com.example.mars_rover_photos.web;
 
+import com.example.mars_rover_photos.response.ApiResponseRoverPhotos;
+import com.example.mars_rover_photos.service.MarsRoverAPIService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private final MarsRoverAPIService roverService;
+
+    public HomeController(MarsRoverAPIService roverService) {
+        this.roverService = roverService;
+    }
 
     @GetMapping("/")
     public String home(ModelMap model){
-        model.put("name", "Minnie Mouse");
-        model.put("address", "123 Fake street");
+        ApiResponseRoverPhotos roverData = roverService.getRoverData();
+        model.put("roverData", roverData);
         return "index";
 
     }
