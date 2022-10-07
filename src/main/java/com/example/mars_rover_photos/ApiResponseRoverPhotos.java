764062c53bf30f57ffd6590c7ad08e1edc4f17af
Maren Lilleberre <marenlilleberre@gmail.com>
2022-10-07 11:46:14 +0200

    add MarsPhotos class with variables and getters/setters

---
+++ src/main/java/com/example/mars_rover_photos/ApiResponseRoverPhotos.java
@@ -0,0 +1,8 @@
+package com.example.mars_rover_photos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApiResponseRoverPhotos {
+    List<MarsPhotos> photos = new ArrayList<>();
+}
