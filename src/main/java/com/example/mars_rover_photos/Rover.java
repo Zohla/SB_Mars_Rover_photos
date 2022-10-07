b594acea3453f2fac1ff827ce088840ff9c24940
Maren Lilleberre <marenlilleberre@gmail.com>
2022-10-07 11:46:49 +0200

    add Rover class with API corresponding variables

---
+++ src/main/java/com/example/mars_rover_photos/Rover.java
@@ -0,0 +1,9 @@
+package com.example.mars_rover_photos;
+
+public class Rover {
+    private int roverId;
+    private String roverName;
+    private String landingDate;
+    private String launchDate;
+    private String status;
+}
