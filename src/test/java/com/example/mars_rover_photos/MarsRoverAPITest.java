6ade51696f5a8c6a02acf7a8f3c1aa21d6ef4a92
Maren Lilleberre <marenlilleberre@gmail.com>
2022-10-07 11:32:26 +0200

    add API-test

---
+++ src/test/java/com/example/mars_rover_photos/MarsRoverAPITest.java
@@ -0,0 +1,17 @@
+package com.example.mars_rover_photos;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class MarsRoverAPITest {
+
+    @Test
+    public void smallTest() {
+        RestTemplate rt = new RestTemplate();
+
+        ResponseEntity<String> response = rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?id=118330&sol=100&api_key=7C3efywnhiZulYlDVdUYW9M5u71tIS22nmcak61h", String.class);
+        System.out.println(response.getBody());
+    }
+}
