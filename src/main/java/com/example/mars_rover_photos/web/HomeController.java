8335298fc4298362f578e11aed17e4dab20716a6
Maren Lilleberre <marenlilleberre@gmail.com>
2022-10-09 16:00:33 +0200

    add PostMapping to update pictures according to which button was clicked

--- src/main/java/com/example/mars_rover_photos/web/HomeController.java
+++ src/main/java/com/example/mars_rover_photos/web/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -20,7 +22,13 @@ public class HomeController {
         ApiResponseRoverPhotos roverData = roverService.getRoverData("curiosity");
         model.put("roverData", roverData);
         return "index";
+    }
 
+    @PostMapping("/")
+    public String postHomeView(ModelMap model, @RequestParam String marsApiRoverData){
+        ApiResponseRoverPhotos roverData = roverService.getRoverData(marsApiRoverData);
+        model.put("roverData", roverData);
+        return "index";
     }
 
 }
