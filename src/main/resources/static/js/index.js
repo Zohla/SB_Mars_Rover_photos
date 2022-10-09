171700caefb66b388ff6a9f0c4af895380dbaf46
Maren Lilleberre <marenlilleberre@gmail.com>
2022-10-09 15:59:38 +0200

    add eventlisteners to btns and get rovername

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,13 @@
 
-document.getElementById("curiosity").addEventListener('click',
-    function () {
-        alert("Curiosity was clicked!");
+const marsApiButtons = document.querySelectorAll("button[id*='marsApi']");
+
+for (let i = 0;i<marsApiButtons.length;i++){
+    let currentRoverBtn = marsApiButtons[i];
+    currentRoverBtn.addEventListener('click', function(){
+        //alert(currentRoverBtn.innerHTML + " clicked!")
+        let apiData = document.getElementById("marsApiRoverData");
+        apiData.value = currentRoverBtn.innerHTML;
+        document.getElementById('frmRoverType').submit();
+
     })
+}
