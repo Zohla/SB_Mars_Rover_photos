cf4ef69fe922fd2057881a7572e4bcf08567585f
Maren Lilleberre <marenlilleberre@gmail.com>
2022-10-13 10:30:10 +0200

    add background color to active button

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,13 +1,23 @@
 
 const marsApiButtons = document.querySelectorAll("button[id*='marsApi']");
+let urlParams = new URLSearchParams(window.location.search);
+let displayedRover = urlParams.get("marsApiRoverData")
 
+for (let i = 0;i<marsApiButtons.length;i++){
+    if (marsApiButtons[i].innerHTML.toLowerCase() == displayedRover.toLowerCase()){
+        marsApiButtons[i].style.backgroundColor = 'orange';
+    } else{
+        marsApiButtons[i].style.backgroundColor = '#0d6efd';
+    }
+}
 for (let i = 0;i<marsApiButtons.length;i++){
     let currentRoverBtn = marsApiButtons[i];
     currentRoverBtn.addEventListener('click', function(){
-        //alert(currentRoverBtn.innerHTML + " clicked!")
         let apiData = document.getElementById("marsApiRoverData");
         apiData.value = currentRoverBtn.innerHTML;
         document.getElementById('frmRoverType').submit();
-
+        currentRoverBtn.style.backgroundColor ='orange';
     })
 }
+
+
