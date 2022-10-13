d52d430210ea8770ab655ba017e35be451932953
Maren Lilleberre <marenlilleberre@gmail.com>
2022-10-13 11:56:38 +0200

    add check if displayedRover is null

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -4,19 +4,23 @@ let urlParams = new URLSearchParams(window.location.search);
 let displayedRover = urlParams.get("marsApiRoverData")
 
 for (let i = 0;i<marsApiButtons.length;i++){
+    if (displayedRover == null){
+        displayedRover = "Curiosity";
+    }
     if (marsApiButtons[i].innerHTML.toLowerCase() == displayedRover.toLowerCase()){
-        marsApiButtons[i].style.backgroundColor = 'orange';
+        marsApiButtons[i].classList.remove("btn-secondary");
+        marsApiButtons[i].classList.add("btn-primary");
     } else{
-        marsApiButtons[i].style.backgroundColor = '#0d6efd';
+        marsApiButtons[i].classList.add("btn-secondary");
+        marsApiButtons[i].classList.remove("btn-primary");
     }
 }
 for (let i = 0;i<marsApiButtons.length;i++){
     let currentRoverBtn = marsApiButtons[i];
-    currentRoverBtn.addEventListener('click', function(){
+    currentRoverBtn.addEventListener('click', function() {
         let apiData = document.getElementById("marsApiRoverData");
         apiData.value = currentRoverBtn.innerHTML;
         document.getElementById('frmRoverType').submit();
-        currentRoverBtn.style.backgroundColor ='orange';
     })
 }
 
